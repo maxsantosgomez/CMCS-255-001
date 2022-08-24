a3b7baeaa9b8606b166f56b12b05a2f376666d47
maxsantosgomez <112021232+maxsantosgomez@users.noreply.github.com>
2022-08-24 17:14:41 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*******************************************************
+* Hello.Java
+********************************************************
+* Max Santos Gomez
+* 08-24-2022
+* CMSC-255-001
+********************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("<Max Santos Gomez>");
+		System.out.println("<Hello.Java>");
+		System.out.println("<Write your name in Java>");
+		System.out.println("<08-24-2022>");
+		System.out.println("<CMSC-255-001>");
+		System.out.println();
+
+	}
+}
